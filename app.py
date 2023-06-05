025d7a8f9f1956e7cdcd8f9de7e4aeeaaec53bb2
Adrian Wesolowski <wrx75747@student.wsb.wroclaw.pl>
2023-06-05 09:03:58 +0200

    Poprawione Lab1 o tym co wspominała Pani profesor w komentarzu

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
  return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
-=======
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
- return '<h1>Hello WSB! Greetings from Flask!</h1>'
-if __name__ == "__main__":
->>>>>>> f7aa38272327614277e5232a6f373649e8b7f08d
  app.run(debug=True)
