21931be7ca114cabab6641c259d3ba46fd28d93d
root <root@DESKTOP-TI1MPDV>
2023-06-20 08:33:19 +0200

    Dodanie pliku .travis.yaml

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
-from flask import Flask, render_template
-from markupsafe import escape
+from flask import Flask
 app = Flask(__name__)
-
-
 @app.route('/')
 def index():
-<<<<<<< HEAD
-    return render_template('index.html')
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    your_name = "Adrian"
-    return render_template('hello.html', name=your_name)
-=======
  return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
 if __name__ == "__main__":
  app.run(debug=True)
->>>>>>> b98ed1f974cbbe668c3bc001d0e1c5cd059ded4a
